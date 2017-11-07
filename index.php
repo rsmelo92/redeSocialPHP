d7c37f2c836fed53ec2d01bf034bbee98961909a
rsmeloba <rsmelo_@hotmail.com>
2017-11-06 23:22:29 -0300

    Agora vai

--- index.php
+++ index.php
@@ -3,11 +3,18 @@
 	error_reporting(1);
 
 	session_start("rede_social");
-
+	// local
 	$servidor 	= "localhost";
 	$user 		= "root";
 	$senha 		= "";
 	$banco	 	= "andrecos_unifacs";
+
+	// heroku
+	$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+	$servidor = $url["host"];
+	$user 	  = $url["user"];
+	$senha 	  = $url["pass"];
+	$banco 	  = substr($url["path"], 1);
 
 	$con = new mysqli($servidor, $user, $senha, $banco);
 
